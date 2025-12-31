22847aa0daeaae886147b4c083c558ae1cdb5c4e
Afnankazi <afnanarifkazi2@gmail.com>
2025-12-31 18:18:13 +0530

    fixed notification issues

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -22,11 +22,11 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message', payload);
   
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/firebase-logo.png'
-  };
+  // const notificationTitle = payload.notification.title;
+  // const notificationOptions = {
+  //   body: payload.notification.body,
+  //   icon: '/firebase-logo.png'
+  // };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  // self.registration.showNotification(notificationTitle, notificationOptions);
 });
